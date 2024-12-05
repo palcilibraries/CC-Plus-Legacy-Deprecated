5ce33ce02cc22843aa3b66baecffebf04eb1d053
Scott Ross <lsr2912@gmail.com>
2024-12-05 13:46:09 -0500

    Change passed value for providers with null "updated_at" from null to an empty string

--- app/Http/Controllers/GlobalAdminController.php
+++ app/Http/Controllers/GlobalAdminController.php
@@ -82,7 +82,7 @@ class GlobalAdminController extends Controller
             $parsedUrl = parse_url($gp->server_url_r5);
             $provider['host_domain'] = (isset($parsedUrl['host'])) ? $parsedUrl['host'] : "-missing-";
             $provider['connections'] = $connections;
-            $provider['updated'] = (is_null($gp->updated_at)) ? null : date("Y-m-d h:ia", strtotime($gp->updated_at));
+            $provider['updated'] = (is_null($gp->updated_at)) ? "" : date("Y-m-d h:ia", strtotime($gp->updated_at));
             $providers[] = $provider;
         }
 
